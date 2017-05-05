cc43bfae65ea3a0ccad70e76d41d53b6fce4d18c
chenhfnn <chenhfnn@163.com>
2017-05-05 17:51:53 +0800

    fdfasfasfafafadsfasfasfas

---
+++ src/main/java/com/chenhf/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.chenhf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+        System.out.println("111");
+        SpringApplication.run(SpringbootApplication.class, args);
+	}
+}
